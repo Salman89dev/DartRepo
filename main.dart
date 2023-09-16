592550e285e7913e9ddc97f3cc3399c7ef8e3f92
salman89dev <hmsalmansadiq@gmail.com>
2023-09-16 15:18:52 +0500

    today's final Commit

--- main.dart
+++ main.dart
@@ -1,11 +1,19 @@
-void main()
-{
-  print("hello");
-  for(int i=0;i<10;i++)
-  {
-    print(i);
-  }
-}
+// void main()
+// {
+//   var num=[1,2,3,4,5,6,7,2,3,4,8,8,9];
+//   var newNum=[];
+//   for(int i=0;i<num.length;i++)
+//   {
+//     for(int j=num.length-1;j>0;j--)
+//     {
+//       if(num[i] != newNum[j])
+//       {
+//         newNum.add(num[j]);
+//       }
+//     }
+//   }
+//   print(newNum);
+// }
 
 
 
@@ -15,3 +23,36 @@ void main()
 // git remote add origin
 // git commit -m "first commit"
 // git push -u origin master 
+
+// void main()
+// {
+  // var persaonalData={"name":"Salman","father-Name":"Sadiq"};
+  // var classData={"Class":"X","Roll Number":"B171010"};
+  // persaonalData.addAll(classData);
+  // print(persaonalData);
+
+  // var f1={1: "Apple",2:"Banana"};
+  // var f2={3:"Mango"};
+  // var f3={4:"Pineapple",5:"Strawberry"};
+  // var fruits={}..addAll(f1)..addAll(f2)..addAll(f3);
+  // print(fruits);
+
+  // var fruit={...f1,...f2,...f3};
+  // print(fruit);
+
+  // var list01=["name","fname","age"];
+  // var list02=["Salman","Sadiq",20];
+  // var personal=Map.fromIterables(list01,list02);
+  // print(personal);
+
+  
+
+// }
+
+void main()
+{
+  for(int i=1; i<=10;i++)
+  {
+    print("10 x ${i} = ${i*10}");
+  }
+}
